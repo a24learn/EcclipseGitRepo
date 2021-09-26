38832fce886f3ea8d9d27b8167008a3857bbdd16
aruneshm <a24learn@gmail.com>
2021-09-26 12:13:06 +0530

    commit after dev1 changes

--- src/com/example/MyGit.java
+++ src/com/example/MyGit.java
@@ -4,7 +4,7 @@ public class MyGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("change from dev1");
+		System.out.println("change from dev1 a");
 
 	}
 
